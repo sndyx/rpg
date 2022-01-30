2f37f2873f6fc9dab6ef131abe0cfafde70e2f81
Sandy <sandy@sndy.moe>
2022-01-30 15:13:05 -0500

    Stuff

---
+++ src/main/kotlin/net/aradiata/utility/Nbt.kt
@@ -0,0 +1,36 @@
+package net.aradiata.utility
+
+import net.aradiata.plugin
+import org.bukkit.NamespacedKey
+import org.bukkit.inventory.meta.ItemMeta
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
+
+class PrimitivePersistentDataType<T>(
+    private val primitiveType: Class<T>
+) : PersistentDataType<T, T> {
+   
+    override fun getPrimitiveType(): Class<T> = primitiveType
+    
+    override fun getComplexType(): Class<T> = primitiveType
+    
+    override fun toPrimitive(
+        complex: T,
+        context: PersistentDataAdapterContext
+    ): T = complex
+    
+    override fun fromPrimitive(
+        primitive: T,
+        context: PersistentDataAdapterContext
+    ): T = primitive
+    
+}
+
+inline fun <reified T> PersistentDataContainer.get(
+    key: String,
+): T? = get(NamespacedKey(plugin, key), PrimitivePersistentDataType(T::class.java))
+
+inline fun <reified T> ItemMeta.getNbt(
+    key: String
+): T? = persistentDataContainer.get(key)
