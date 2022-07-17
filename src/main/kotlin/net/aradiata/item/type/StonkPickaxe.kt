ec9ad421ac126e9d218b7480af269dfa151fde44
Sandy <sandy@sndy.moe>
2022-07-17 16:59:05 -0400

    Long awaited stonk buff

--- src/main/kotlin/net/aradiata/item/type/StonkPickaxe.kt
+++ src/main/kotlin/net/aradiata/item/type/StonkPickaxe.kt
@@ -15,7 +15,7 @@ object StonkPickaxe : Pickaxe {
             " joke."
     
     override val power: Int = 20
-    override val speed: Int = 50
+    override val speed: Int = 200
     override val dropIncrease: Int = 1000
     
 }
